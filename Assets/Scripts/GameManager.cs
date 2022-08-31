536bcd56f6cc9aafb5820856b6e6d19cf3e04736
MaxsterD <aexandertl810@gmail.com>
2022-08-30 22:10:05 -0500

    Portal, Text Manager, Coin chest

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //References
+    public Player player;
+
+    public FloatingTextManager floatingTextManager;
+
+    public int pesos;
+    public int experience;
+
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+
+    }
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+
+        PlayerPrefs.SetString("saveState", s);
+    }
+
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        //Change Skin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //Change weapon level
+        Debug.Log("Load");
+    }
+}
